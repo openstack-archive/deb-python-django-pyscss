29fc7bee16768768ee91b7a65ee1e6a0ee4bc950
Thomas Goirand <thomas@goirand.fr>
2015-08-04 07:45:44 +0000

    Fixed diff with upstream

--- tests/test_scss.py
+++ tests/test_scss.py
@@ -56,10 +56,6 @@ class ImportTestMixin(CompilerTestMixin):
         actual = self.compiler.compile_string('@import "/css_prefix/baz.scss";')
         self.assertEqual(clean_css(actual), clean_css(FOO_CONTENTS))
 
-    def test_import_from_staticfiles_dirs_prefixed(self):
-        actual = self.compiler.compile(scss_string='@import "/css_prefix/baz.scss";')
-        self.assertEqual(clean_css(actual), clean_css(FOO_CONTENTS))
-
     def test_import_from_staticfiles_dirs_relative(self):
         actual = self.compiler.compile_string('@import "css/foo.scss";')
         self.assertEqual(clean_css(actual), clean_css(FOO_CONTENTS))
